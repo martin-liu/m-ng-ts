f96aa009275e788ed2118dfd77db486283ab657c
Martin Liu <hualiu@ebay.com>
2016-07-19 18:15:55 -0700

    remove useless file

--- app/common/services/require.service.ts
+++
@@ -1,7 +0,0 @@
-declare var require:any;
-export default class RequireService {
-  static requireFolder(folder, regexp) {
-    let req = require.context(folder , true, regexp);
-    req.keys().forEach(req);
-  }
-}
