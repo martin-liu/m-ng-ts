e7ce023c904b3efccf069140139e70830cb873ea
Martin Liu <hualiu@ebay.com>
2017-02-09 19:37:50 -0800

    add es6-promise polyfill for IE support

--- app/core/bootstrap.ts
+++ app/core/bootstrap.ts
@@ -1,3 +1,4 @@
+require('es6-promise').polyfill();
 import 'bootstrap-loader';
 
 import * as angular from 'angular';
--- app/test.index.ts
+++ app/test.index.ts
@@ -1,4 +1,3 @@
-require('es6-promise').polyfill();
 import './core/bootstrap';
 import 'angular-mocks';
 
